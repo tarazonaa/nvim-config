c0b77332f37bdcd249b66397c0be2caa2ed593c7
Tarazonaa <andres.tara.so@gmail.com>
2026-05-19 22:39:16 -0600

    Some terrifying shit happened

--- after/lsp/apollo.lua
+++
@@ -1,6 +0,0 @@
-vim.lsp.config['apollo'] = {
-  -- Command and arguments to start the server.
-  cmd = { 'rover', 'lsp', '--supergraph-config', 'supergraph.yaml' },
-  -- Filetypes to automatically attach to.
-  filetypes = { 'graphql' },
-}
